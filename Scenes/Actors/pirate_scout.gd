b8d0c28254b26420d81adc0909df98a5f6987a02
Sunny-Days-Ahead <ura.goodgirl.sunny@gmail.com>
2025-11-11 14:42:35 -0600

    Warcrime Simulation
    Added Score

--- Scenes/Actors/pirate_scout.gd
+++ Scenes/Actors/pirate_scout.gd
@@ -6,4 +6,9 @@ func _process(delta: float) -> void:
 
 
 func _on_health_component_died() -> void:
+	var scoreNode : Label = Global.ui_node.find_child("Score")
+	
+	if scoreNode.has_method("addScore"):
+		scoreNode.addScore(1)
+
 	queue_free()
