f30f7da199351aa490bcda85a3bf294dc30e7553
Sunny-Days-Ahead <ura.goodgirl.sunny@gmail.com>
2025-11-13 14:28:49 -0600

    main menu wip

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_begin_mission_toggled(toggled_on: bool) -> void:
+	load
