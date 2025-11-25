220d62f29cabad08e199992506148f6acee09bf1
Sunny-Days-Ahead <ura.goodgirl.sunny@gmail.com>
2025-11-25 16:08:12 -0600

    Created PathFollow and Seeker Pirates
    as templates to make inherited scenes

---
+++ Scenes/Actors/Pirates/pathFollow/pathFollowPirate.gd
@@ -0,0 +1,28 @@
+extends PathFollow2D
+
+@export var points : int = 1
+@export var speed : float = 100.0
+
+func _ready() -> void:
+	%Spawner.spawnFunction = setup_bullet
+
+func _process(delta: float) -> void:
+	progress += speed * delta
+
+func _on_health_component_died() -> void:
+	var scoreNode : Label = Global.ui_node.find_child("Score")
+	
+	if scoreNode.has_method("addScore"):
+		scoreNode.addScore(points)
+
+	queue_free()
+
+# This function sets the initial state of the bullet that the player fires with the spawner.
+func setup_bullet(bulletNode : Node2D) -> void:
+	bulletNode.is_player_bullet = false
+	bulletNode.direction = Vector2.DOWN
+	bulletNode.speed = 200.0
+	bulletNode.damage = 1
+
+func _on_spawner_spawned(entity: Node) -> void:
+	%BulletSFX.playJitter()
---
+++ Scenes/Actors/Pirates/seeker/seekerPirate.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var points : int = 1
+
+@export_category("Speed")
+@export var speed : float = 50.0
+@export var maxSpeed : float = 400.0
+@export var speedRamp : float = 225.0
+
+var desiredDirection := Vector2(0, 0)
+var desiredRotation : float
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	speed += speedRamp * delta
+	if speed > maxSpeed:
+		speed = maxSpeed
+	
+	desiredDirection = Global.player_node.global_position - global_position
+	desiredDirection = desiredDirection.normalized()
+	global_position += desiredDirection * speed * delta
+	
+	desiredRotation = desiredDirection.angle()  + deg_to_rad(90)
+	global_rotation = desiredRotation 
+
+func _on_health_component_died() -> void:
+	var scoreNode : Label = Global.ui_node.find_child("Score")
+	
+	if scoreNode.has_method("addScore"):
+		scoreNode.addScore(points)
+
+	queue_free()
