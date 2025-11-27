2af21d907b9a8e0aaf2e5d1f04047b73f2f3d2a5
Sunny-Days-Ahead <ura.goodgirl.sunny@gmail.com>
2025-11-26 18:03:00 -0600

    make asteroids pick a random degree of rotation

--- Scenes/Actors/Asteroid/asteroid.gd
+++ Scenes/Actors/Asteroid/asteroid.gd
@@ -1,14 +1,18 @@
 extends "res://Scenes/Actors/Pirates/seeker/seekerPirate.gd"
+var angle
 	
+func _ready() -> void:
+	angle = randf_range(-45, 45)
+
 func _process(delta: float) -> void:
 	global_position.y += speed * delta
-	global_rotation_degrees += 30 * delta
-	 
+	global_rotation_degrees += angle * delta
+
 func _on_health_component_died() -> void:
 	$HurtBox.queue_free()
 	$HitBox.queue_free()
 	%Sprite2D.hide()
 	%Explodes.play()
-	
+
 func _on_explodes_animation_finished() -> void:
 	queue_free()
